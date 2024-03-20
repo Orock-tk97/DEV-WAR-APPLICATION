ce74415b555eb8ada167060046bc241570c31a5f
Orocktk <kingsagbor97@gmail.com>
2024-03-20 02:29:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from "@material-tailwind/react";
+
+
+function App() {
+  
+
+  return (
+<div>
+  DEV_WAR_APPLICATION
+</div>
+  )
+}
+
+export default App
